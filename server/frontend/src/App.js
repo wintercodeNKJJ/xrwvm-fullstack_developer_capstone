8f960dbcc8a741b01f3e4675fe7b2e508e06ea9b
wintercode <kenfackjordanjunior@gmail.com>
2026-03-15 02:14:12 +0100

    deployment next

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,7 +2,8 @@ import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
